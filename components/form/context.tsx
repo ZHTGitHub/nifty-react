8bc9be46cb1688d94d7bb4e804d6dccd0ba16ada
ZHT <613547046@qq.com>
2024-06-26 14:39:45 +0800

    ci: 开发表单组件

--- components/form/context.tsx
+++ components/form/context.tsx
@@ -7,6 +7,10 @@ export interface FormContextProps {
   colon?: boolean;
   labelAlign?: FormLabelAlign;
   labelCol?: ColProps;
+  onValueChange?: (key: string, value: any) => void;
+  setValues?: (values: Record<string, any>) => void;
+  validateRegister?: (name:string, cb: Function) => void;
+  values?: Record<string, any>;
   vertical: boolean;
   wrapperCol?: ColProps;
 }
